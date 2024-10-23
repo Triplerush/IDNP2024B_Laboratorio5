336af2f817ee298bb616404d7c1b1b3aea3795f0
Triplerush <fcanal@unsa.edu.pe>
2024-10-23 16:25:00 -0500

    Recreando el activity principal

---
+++ app/src/main/java/com/example/laboratorio5/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.laboratorio5;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+}
